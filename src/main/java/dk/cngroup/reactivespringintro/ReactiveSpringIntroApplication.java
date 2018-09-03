1d52e7cc2b4f382616da152c36bf1020bc41c83e
Adam Kučera <kucera@cngroup.dk>
2018-09-03 07:32:05 +0200

    Refactor to package private

--- src/main/java/dk/cngroup/reactivespringintro/ReactiveSpringIntroApplication.java
+++ src/main/java/dk/cngroup/reactivespringintro/ReactiveSpringIntroApplication.java
@@ -1,5 +1,7 @@
 package dk.cngroup.reactivespringintro;
 
+import static dk.cngroup.reactivespringintro.ReactiveSpringIntroApplication.randomId;
+
 import java.time.Duration;
 import java.util.UUID;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,133 +20,142 @@ import reactor.core.publisher.Mono;
 @SpringBootApplication
 public class ReactiveSpringIntroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveSpringIntroApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveSpringIntroApplication.class, args);
+  }
 
-	@RestController
-	public class JavaMeetupController {
-		private final JavaMeetupService service;
+  public static String randomId() {
+    return UUID.randomUUID().toString();
+  }
 
-		public JavaMeetupController(
-				JavaMeetupService service) {
-			this.service = service;
-		}
+}
 
-		@GetMapping("devs")
-		public Flux<JavaDev> getDevs() {
-			return service.developers();
-		}
+@RestController
+class JavaMeetupController {
 
-		@GetMapping("devs/{id}")
-		public Mono<JavaDev> getDev(@PathVariable("id") String id) {
-			return service.byId(id);
-		}
+  private final JavaMeetupService service;
 
-		@GetMapping("devs/{id}/consumption")
-		public Flux<Consumption> getConsumption(@PathVariable("id") String id) {
-			return service.consumptionForDev(id);
-		}
-	}
+  public JavaMeetupController(
+      JavaMeetupService service) {
+    this.service = service;
+  }
 
-	@Service
-	public class JavaMeetupService {
-		private final JavaMeetupRepository repository;
+  @GetMapping("devs")
+  public Flux<JavaDev> getDevs() {
+    return service.developers();
+  }
 
-		public JavaMeetupService(JavaMeetupRepository repository) {
-			this.repository = repository;
-		}
+  @GetMapping("devs/{id}")
+  public Mono<JavaDev> getDev(@PathVariable("id") String id) {
+    return service.byId(id);
+  }
 
-		public Flux<Consumption> consumptionForDev(String id) {
-			return Flux.interval(Duration.ofSeconds(1))
-					.map(second -> new Consumption(randomId(), Substance.random(), id));
-		}
+  @GetMapping("devs/{id}/consumption")
+  public Flux<Consumption> getConsumption(@PathVariable("id") String id) {
+    return service.consumptionForDev(id);
+  }
+}
 
-		public Mono<JavaDev> byId(String id) {
-			return repository.findById(id);
-		}
+@Service
+class JavaMeetupService {
 
-		public Flux<JavaDev> developers() {
-				return repository.findAll();
-		}
-	}
+  private final JavaMeetupRepository repository;
 
-	private String randomId() {
-		return UUID.randomUUID().toString();
-	}
+  public JavaMeetupService(JavaMeetupRepository repository) {
+    this.repository = repository;
+  }
 
-	interface JavaMeetupRepository extends ReactiveMongoRepository<JavaDev, String> {}
+  public Flux<Consumption> consumptionForDev(String id) {
+    return Flux.interval(Duration.ofSeconds(1))
+        .map(second -> new Consumption(randomId(), Substance.random(), id));
+  }
+
+  public Mono<JavaDev> byId(String id) {
+    return repository.findById(id);
+  }
+
+  public Flux<JavaDev> developers() {
+    return repository.findAll();
+  }
+}
 
 
-	@Document
-	public class JavaDev {
-		@Id
-		private String id;
-		private String name;
+@Repository
+interface JavaMeetupRepository extends ReactiveMongoRepository<JavaDev, String> {
 
-		public String getId() {
-			return id;
-		}
+}
 
-		public void setId(String id) {
-			this.id = id;
-		}
 
-		public String getName() {
-			return name;
-		}
+@Document
+class JavaDev {
 
-		public void setName(String name) {
-			this.name = name;
-		}
-	}
+  @Id
+  private String id;
+  private String name;
 
-	@Document
-	public class Consumption {
-		@Id
-		private String id;
-		private Substance substance;
-		private String javaDevId;
+  public String getId() {
+    return id;
+  }
 
-		public Consumption(String id,
-				Substance substance,
-				String javaDevId) {
-			this.id = id;
-			this.substance = substance;
-			this.javaDevId = javaDevId;
-		}
+  public void setId(String id) {
+    this.id = id;
+  }
 
-		public String getId() {
-			return id;
-		}
+  public String getName() {
+    return name;
+  }
 
-		public void setId(String id) {
-			this.id = id;
-		}
+  public void setName(String name) {
+    this.name = name;
+  }
+}
 
-		public Substance getSubstance() {
-			return substance;
-		}
+@Document
+class Consumption {
 
-		public void setSubstance(
-				Substance substance) {
-			this.substance = substance;
-		}
+  @Id
+  private String id;
+  private Substance substance;
+  private String javaDevId;
 
-		public String getJavaDevId() {
-			return javaDevId;
-		}
+  public Consumption(String id,
+      Substance substance,
+      String javaDevId) {
+    this.id = id;
+    this.substance = substance;
+    this.javaDevId = javaDevId;
+  }
 
-		public void setJavaDevId(String javaDevId) {
-			this.javaDevId = javaDevId;
-		}
-	}
+  public String getId() {
+    return id;
+  }
 
-	public enum Substance {
-		COKE, BEER, SAUSAGE, STEAK;
+  public void setId(String id) {
+    this.id = id;
+  }
 
-		public static Substance random() {
-			return null;
-		}
-	}
+  public Substance getSubstance() {
+    return substance;
+  }
+
+  public void setSubstance(
+      Substance substance) {
+    this.substance = substance;
+  }
+
+  public String getJavaDevId() {
+    return javaDevId;
+  }
+
+  public void setJavaDevId(String javaDevId) {
+    this.javaDevId = javaDevId;
+  }
+}
+
+enum Substance {
+  COKE, BEER, SAUSAGE, STEAK;
+
+  public static Substance random() {
+    return null;
+  }
 }
