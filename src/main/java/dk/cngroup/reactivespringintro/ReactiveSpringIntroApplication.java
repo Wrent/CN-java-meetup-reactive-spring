547f42c359d02adff95b94df8de9f272ef7d92c8
Adam Kučera <kucera@cngroup.dk>
2018-09-03 07:21:38 +0200

    Create service

--- src/main/java/dk/cngroup/reactivespringintro/ReactiveSpringIntroApplication.java
+++ src/main/java/dk/cngroup/reactivespringintro/ReactiveSpringIntroApplication.java
@@ -1,9 +1,15 @@
 package dk.cngroup.reactivespringintro;
 
+import java.time.Duration;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ReactiveSpringIntroApplication {
@@ -11,6 +17,35 @@ public class ReactiveSpringIntroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveSpringIntroApplication.class, args);
 	}
+
+	@Service
+	public class JavaMeetupService {
+		private final JavaMeetupRepository repository;
+
+		public JavaMeetupService(JavaMeetupRepository repository) {
+			this.repository = repository;
+		}
+
+		public Flux<Consumption> consumptionForDev(String id) {
+			return Flux.interval(Duration.ofSeconds(1))
+					.map(second -> new Consumption(randomId(), Substance.random(), id));
+		}
+
+		public Mono<JavaDev> byId(String id) {
+			return repository.findById(id);
+		}
+
+		public Flux<JavaDev> developers() {
+				return repository.findAll();
+		}
+	}
+
+	private String randomId() {
+		return UUID.randomUUID().toString();
+	}
+
+	interface JavaMeetupRepository extends ReactiveMongoRepository<JavaDev, String> {}
+
 
 	@Document
 	public class JavaDev {
@@ -40,6 +75,15 @@ public class ReactiveSpringIntroApplication {
 		@Id
 		private String id;
 		private Substance substance;
+		private String javaDevId;
+
+		public Consumption(String id,
+				Substance substance,
+				String javaDevId) {
+			this.id = id;
+			this.substance = substance;
+			this.javaDevId = javaDevId;
+		}
 
 		public String getId() {
 			return id;
@@ -57,9 +101,21 @@ public class ReactiveSpringIntroApplication {
 				Substance substance) {
 			this.substance = substance;
 		}
+
+		public String getJavaDevId() {
+			return javaDevId;
+		}
+
+		public void setJavaDevId(String javaDevId) {
+			this.javaDevId = javaDevId;
+		}
 	}
 
 	public enum Substance {
 		COKE, BEER, SAUSAGE, STEAK;
+
+		public static Substance random() {
+			return null;
+		}
 	}
 }
