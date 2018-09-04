a0d6d9bddd3a418ee7938d7968b6f0e97f94390a
Adam Kučera <kucera@cngroup.dk>
2018-09-04 08:15:16 +0200

    Add client

---
+++ client/src/main/java/dk/cngroup/reactivespringintroclient/ReactiveSpringIntroClientApplication.java
@@ -0,0 +1,12 @@
+package dk.cngroup.reactivespringintroclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringIntroClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringIntroClientApplication.class, args);
+	}
+}
---
+++ client/src/test/java/dk/cngroup/reactivespringintroclient/ReactiveSpringIntroClientApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.cngroup.reactivespringintroclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveSpringIntroClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
